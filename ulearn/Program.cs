62244b62bf8f67857b18b25e69f8690cbcc179e3
Дашике <@DESKTOP-2UAEPM4>
2023-11-14 12:59:31 +0700

    Добавьте файлы проекта.

---
+++ ulearn/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ulearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N=");
+            int N = int.Parse(Console.ReadLine());
+            int[] number = new int[N];
+            FillArray(number);
+
+            PrintArray(number);
+            int ind = MinIndex(number);
+            Console.WriteLine($"индекс минимального числа: {ind}");
+            
+        }
+        static void PrintArray(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++) 
+            {
+                Console.WriteLine(arr[i]);
+            }
+           
+
+
+        }
+
+        static int[] FillArray(int [] number)
+        {
+            
+            Random rnd = new Random();
+            
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = rnd.Next(-100, 100);
+             
+            }
+            return number;
+        }
+
+      
+        static int MinIndex(int[] array)
+        {
+            int min = array[0];
+            int index = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+          
+        }
+        static int MinElement(int[] array)
+        {
+            int min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+
+            return min;
+        }
+    }
+}
